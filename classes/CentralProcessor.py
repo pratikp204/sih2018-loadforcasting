72086f3a279d02af0aad92ba629ab35b072adb02
shivam ashtikar <lightningshivam@gmail.com>
2018-03-21 16:39:17 +0530

    created abstract model class

---
+++ classes/CentralProcessor.py
@@ -0,0 +1,24 @@
+class CentralProcessor():
+
+    def __init__(self,curent_timestamp,default_api_code,last_schedules):
+        self.curent_timestamp = curent_timestamp
+        self.default_api_code = default_api_code
+        self.last_schedules = last_schedules
+
+    def periodic_prediction_schedular(self):
+        pass
+
+    def periodic_training_schedular(self):
+        pass
+
+    def save_training_module(self):
+        pass
+
+    def load_regressor(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def schedule_training(self):
+        pass
---
+++ classes/Estimator.py
@@ -0,0 +1,19 @@
+class Estimator():
+    def __init__(self,estimation_loss,estimation_accuracy):
+        self.estimation_loss = estimation_loss
+        self.estimation_accuracy = estimation_accuracy
+
+    def get_input(self):
+        pass
+
+    def calculate_error(self):
+        pass
+
+    def update_weight(self):
+        pass
+
+    def predict_output(self):
+        pass
+
+    def show_result(self):
+        pass
