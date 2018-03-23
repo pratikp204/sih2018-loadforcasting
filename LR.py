5ebe18691beff763d8d38e24266bced760f3ca2d
JAY BORA <borajay33@gmail.com>
2018-03-23 18:11:34 +0530

    This is Linear Regression Model

--- LR.py
+++ LR.py
@@ -3,15 +3,24 @@ from sklearn.linear_model import LinearRegression
 from sklearn.feature_selection import mutual_info_regression
 import pandas as pd
 import numpy as np
-from sklearn.decomposition import PCA
 from sklearn import preprocessing
+from scipy.stats import pearsonr
 
 df = pd.read_csv('Constructed datasetV2.csv')
 df = df[24:]
 df['lasthr'] = df.load.shift(+1)
 df = df[1:]
+df['hour2']=df['Hour']**2
+df['hour3']=df['Hour']**3
+df['hour4']=df['Hour']**4
+df['hour5']=df['Hour']**5
+df['t2']=df['T']**2
+df['weekday2']=df['weekday']**2
+df['m2']=df['monthofyear']**2
+df['m3']=df['monthofyear']**3
+df['m4']=df['monthofyear']**4
 train_y = np.asarray(df['load'])
-train_x = np.array(df.drop(['Date','load'],1))
+train_x = np.array(df.drop(['Date','load','prevload','lasthr'],1))
 test_y = train_y[int(len(train_x)*0.9):]
 train_y = train_y[:int(len(train_y)*0.9)]
 
