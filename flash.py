60e9ee0f137aa3f8649051aa49f7c33077c917a3
shivam ashtikar <lightningshivam@gmail.com>
2018-03-24 18:31:18 +0530

    created graph generator

--- flash.py
+++ flash.py
@@ -1,1 +1,27 @@
-print "run bary run"
+import matplotlib.pyplot as plt
+from statistics import  mean
+import pandas as pd
+
+df = pd.read_csv('Constructed datasetV2.csv')
+
+hours = df['Hour']
+ld = df['load']
+loads = []
+loads.append(0)
+for i in range(1,len(ld)):
+    loads.append(ld[i]-ld[i-1])
+
+
+hourload = {}
+for i in range(1,25):
+    hourload[i] = []
+for i in range(len(hours)):
+    hourload[hours[i]].append(loads[i])
+avg_load = []
+
+for i in range(1,25):
+    avg_load.append(mean(hourload[i]))
+
+plt.plot(range(1,25),avg_load)
+plt.legend()
+plt.show()
