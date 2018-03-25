8e5ba53cceda7e1b070766f89127ea0e3316f110
pratik patil <pratikp204@gmail.com>
2018-03-25 22:42:21 +0530

    added by pratik

--- classes/FetchDataUnit.py
+++ classes/FetchDataUnit.py
@@ -3,6 +3,7 @@ from bson import Binary
 import datetime
 
 class FetchDataUnit:
+
     def __init__(self):
         self.client = MongoClient(host='192.168.0.223')
 
@@ -22,19 +23,12 @@ class FetchDataUnit:
             val['lasthr'].append(doc['lasthr'])
         return val
 
-    def storeData(self,pickleobj,zone):
-        da = datetime.datetime.now()
-        db = self.client.picklestore
-        col = db['zone'+str(zone)]
-        dic = {'_id':'{0}{1}{2}{3}'.format(da.day,da.month,da.year,da.hour),'obj':Binary(pickleobj)}
-        col.insert(dic)
-
     def get_obj(self,name,zone):
         oid = str(name)
         db = self.client.picklestore
         col = db['zone' + str(zone)]
         dic = col.find({'_id':oid})
-        return dic['obj']
+        return dic['obj'],dic['preprocessing']
 
     def storeObj(self, pickleobj, zone, acc, preobj, name='{0}/{1}/{2}{3}'.format(datetime.datetime.now().day, datetime.datetime.now().month, datetime.datetime.now().year, datetime.datetime.now().hour)):
         db = self.client.picklestore
@@ -42,18 +36,17 @@ class FetchDataUnit:
         dic = {'_id': name, 'obj': Binary(pickleobj), 'accuracy': acc,'preprocessing':preobj,'zone':zone}
         col.insert(dic)
 
-    def setCurrentObj(self, obj, zone, name):
+    def setCurrentObj(self, obj, zone, name,preobj):
         db = self.client.picklestore
         col = db['currentWrkObj']
-        col.update({'_id': zone}, {'$set': {'_id': zone, 'obj': Binary(obj), 'name': name}}, {'$upsert': True})
+        col.update({'_id': zone}, {'$set': {'_id': zone, 'obj': Binary(obj), 'name': name,'preprocessing':preobj}}, {'$upsert': True})
 
     def get_current_obj(self,zone):
         db = self.client.picklestore
         col = db['currentWrkObj']
         val = col.find({'zone': zone})
-        return val
+        return val['obj'],val['preprocessing']
 
 
 if __name__ == '__main__':
     s = FetchDataUnit()
-    #print s.getTrainData(1)
--- classes/Regression.py
+++ classes/Regression.py
@@ -10,8 +10,9 @@ import numpy as np
 from sklearn import decomposition,svm
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.neural_network import MLPRegressor as mlpr
-from scipy.stats import pearsonr
+#from scipy.stats import pearsonr
 from pickle import load,dumps
+
 class Regression():
 
     def __init__(self):
@@ -47,26 +48,26 @@ class Regression():
         pca.fit(train_x)
         train_x = pca.transform(train_x)
         x_train, x_test, y_train, y_test = cross_validation.train_test_split(train_x, train_y, test_size=0.2)
-        return x_train,x_test,y_train,y_test,scaler
+        return x_train,x_test,y_train,y_test,scaler,pca
     def create_model(self):
         pass
 
-    def train(self,input_set,zone,algo_list=[1],hidden_layer_size=(4),n_jobs=1,kernel='rbf',n_components=15,n_estimators=50,loss='linear',learning_rate=1.0):
+    def train(self,zone,algo_list=[1],hidden_layer_size=(4),n_jobs=1,kernel='rbf',n_components=15,n_estimators=50,loss='linear',learning_rate=1.0):
         input_set=fd.getTrainData(zone)
-        x_train, x_test, y_train, y_test,scaler =self.read_dataset(input_set,n_components)
+        x_train, x_test, y_train, y_test,scaler,pca =self.read_dataset(input_set,n_components)
         for num in algo_list:         
             if num==1:
                 #Linear Regression
                 clf=LinearRegression(n_jobs=n_jobs)
                 clf.fit(x_train,y_train)
                 # storeObj(clf,zone,clf.score(x_test,y_test),'Linear Regression')
-                return clf,zone,clf.score(x_test,y_test),'Linear Regression',scaler
+                return clf,zone,clf.score(x_test,y_test),'Linear Regression',scaler,pca
             elif num==2:
                 # SVR sigmoid
                 clf=svm.SVR(kernel=kernel)
                 clf.fit(x_train,y_train)
                 # storeObj(clf, zone, clf.score(x_test, y_test), 'SVR'+','+kernel)
-                return clf,zone,clf.score(x_test,y_test),'SVR'+kernel,scaler
+                return clf,zone,clf.score(x_test,y_test),'SVR'+kernel,scaler,pca
             elif num==3:
                 #Neural Net
                 clf=mlpr(hidden_layer_size=hidden_layer_size)
@@ -75,30 +76,32 @@ class Regression():
                 for i in hidden_layer_size:
                     str+='-> {}'.format(i)
                 # storeObj(clf, zone, clf.score(x_test, y_test), 'NeuralNet'+' hidden layer size'+hidden_layer_size)
-                return clf,zone,clf.score(x_test,y_test),'NeuralNet hidden_size'+str,scaler
+                return clf,zone,clf.score(x_test,y_test),'NeuralNet hidden_size'+str,scaler,pca
             elif num==4:
                 #Gradient Boosting Regressor
                 clf=GBR(loss=loss,n_estimators=n_estimators,learning_rate=learning_rate)
                 clf.fit(x_train,y_train)
                 # storeObj(clf, zone, clf.score(x_test, y_test), 'Gradient Boosting Regressor')
-                return clf,zone,clf.score(x_test,y_test),'Gradient Boosted Regressor',scaler
+                return clf,zone,clf.score(x_test,y_test),'Gradient Boosted Regressor',scaler,pca
             elif num==5:
                 clf=ABR()
                 clf.fit(x_train,y_train)
                 # storeObj(clf, zone, clf.score(x_test, y_test), 'AdaBoost Regressor')
-                return clf,zone,clf.score(x_test,y_test),'AdaBoost Regressor',scaler
+                return clf,zone,clf.score(x_test,y_test),'AdaBoost Regressor',scaler,pca
 
     def test(self,model):
         pass
 
     @staticmethod
-    def save_model(self,obj,zone,score,preobj,name):
+    def save_model(self,obj,zone,score,preobj,pca,name):
         du=dumps(obj)
-        fd.storeObj(pickleobj=du,zone=zone,acc=score,preobj=preobj,name=name)
+        fd.storeObj(pickleobj=du,zone=zone,acc=score,preobj=preobj,pca=pca,name=name)
 
     @staticmethod
     def predict(self,input_x,name,zone):
-        obj=fd.get_obj(name,zone)
+        obj,preobj,pca=fd.get_obj(name,zone)
+        input_x=preobj.transform(input_x)
+        input_x=pca.transform(input_x)
         return obj.predict(input_x)
 
 
