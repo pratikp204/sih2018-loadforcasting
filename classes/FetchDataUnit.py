dff91545fd7b4ffb5d110930eb5d99f8c2549a33
pratik patil <pratikp204@gmail.com>
2018-03-25 22:13:57 +0530

    added by pratik

--- classes/FetchDataUnit.py
+++ classes/FetchDataUnit.py
@@ -1,14 +1,15 @@
 from pymongo import MongoClient
 from bson import Binary
-import datetime.datetime
-client = MongoClient(host='192.168.0.230')
-db = client.zones
+import datetime
 
+class FetchDataUnit:
+    def __init__(self):
+        self.client = MongoClient(host='192.168.0.223')
 
-class FetchData:
-    @staticmethod
     def getTrainData(self,zone):
-        val = {'temp':[],'date':[],'hour':[],'month':[],'weekday':[],'load':[]}
+        client=self.client
+        db = client.zones
+        val = {'temp':[],'date':[],'hour':[],'month':[],'weekday':[],'load':[],'prevload':[],'lasthr':[]}
         curs=db['zone'+str(zone)].find()
         for doc in curs:
             val['temp'].append(doc['temp'])
@@ -17,33 +18,42 @@ class FetchData:
             val['load'].append(doc['load'])
             val['date'].append(doc['date'])
             val['month'].append(doc['month'])
+            val['prevload'].append(doc['prevload'])
+            val['lasthr'].append(doc['lasthr'])
         return val
-    @staticmethod
-    def storeObj(self,pickleobj,zone,acc,name='{0}{1}{2}{3}'.format(da.day,da.month,da.year,da.hour)):
-        da = datetime.datetime.now()
-        db = client.picklestore
-        col = db['zone'+str(zone)]
-        dic = {'_id':name,'obj':Binary(pickleobj),'accuracy':acc}
-        col.insert(dic)
-    @staticmethod
-    def setCurrentObj(self,obj,zone,name):
-        db = client.picklestore
-        col = db['currentWrkObj']
-        col.update({'_id':zone},{'$set':{'_id':zone,'obj':Binary(obj),'name':name}},{'$upsert':True})
 
-    @staticmethod
-    def get_current_obj(zone):
-        db = client.picklestore
-        col = db['currentWrkObj']
-        val = col.find({'zone':zone})
-        return val
-    @staticmethod
-    def get_obj(name,zone):
+    def storeData(self,pickleobj,zone):
+        da = datetime.datetime.now()
+        db = self.client.picklestore
+        col = db['zone'+str(zone)]
+        dic = {'_id':'{0}{1}{2}{3}'.format(da.day,da.month,da.year,da.hour),'obj':Binary(pickleobj)}
+        col.insert(dic)
+
+    def get_obj(self,name,zone):
         oid = str(name)
-        db = client.picklestore
+        db = self.client.picklestore
         col = db['zone' + str(zone)]
         dic = col.find({'_id':oid})
         return dic['obj']
 
+    def storeObj(self, pickleobj, zone, acc, preobj, name='{0}/{1}/{2}{3}'.format(datetime.datetime.now().day, datetime.datetime.now().month, datetime.datetime.now().year, datetime.datetime.now().hour)):
+        db = self.client.picklestore
+        col = db['zone' + str(zone)]
+        dic = {'_id': name, 'obj': Binary(pickleobj), 'accuracy': acc,'preprocessing':preobj,'zone':zone}
+        col.insert(dic)
+
+    def setCurrentObj(self, obj, zone, name):
+        db = self.client.picklestore
+        col = db['currentWrkObj']
+        col.update({'_id': zone}, {'$set': {'_id': zone, 'obj': Binary(obj), 'name': name}}, {'$upsert': True})
+
+    def get_current_obj(self,zone):
+        db = self.client.picklestore
+        col = db['currentWrkObj']
+        val = col.find({'zone': zone})
+        return val
+
+
 if __name__ == '__main__':
     s = FetchDataUnit()
+    #print s.getTrainData(1)
--- classes/Regression.py
+++ classes/Regression.py
@@ -8,7 +8,7 @@ from sklearn import svm
 import pandas as pd
 import numpy as np
 from sklearn import decomposition,svm
-from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.neural_network import MLPRegressor as mlpr
 from scipy.stats import pearsonr
 from pickle import load,dumps
@@ -40,31 +40,33 @@ class Regression():
         df['m4'] = df['monthofyear'] ** 4
         train_y=np.asarray(input_set['load'])
         train_x=np.asarray(df.drop(['load','date']))
-        train_x=preprocessing.scale(train_x)
+        scaler=MinMaxScaler()
+        scaler.fit(train_x)
+        train_x=scaler.transform(train_x)
         pca = decomposition.PCA(n_components=n_components)
         pca.fit(train_x)
         train_x = pca.transform(train_x)
         x_train, x_test, y_train, y_test = cross_validation.train_test_split(train_x, train_y, test_size=0.2)
-        return x_train,x_test,y_train,y_test 
+        return x_train,x_test,y_train,y_test,scaler
     def create_model(self):
         pass
 
     def train(self,input_set,zone,algo_list=[1],hidden_layer_size=(4),n_jobs=1,kernel='rbf',n_components=15,n_estimators=50,loss='linear',learning_rate=1.0):
         input_set=fd.getTrainData(zone)
-        x_train, x_test, y_train, y_test =self.read_dataset(input_set,n_components)
+        x_train, x_test, y_train, y_test,scaler =self.read_dataset(input_set,n_components)
         for num in algo_list:         
             if num==1:
                 #Linear Regression
                 clf=LinearRegression(n_jobs=n_jobs)
                 clf.fit(x_train,y_train)
                 # storeObj(clf,zone,clf.score(x_test,y_test),'Linear Regression')
-                return clf,zone,clf.score(x_test,y_test),'Linear Regression'
+                return clf,zone,clf.score(x_test,y_test),'Linear Regression',scaler
             elif num==2:
                 # SVR sigmoid
                 clf=svm.SVR(kernel=kernel)
                 clf.fit(x_train,y_train)
                 # storeObj(clf, zone, clf.score(x_test, y_test), 'SVR'+','+kernel)
-                return clf,zone,clf.score(x_test,y_test),'SVR'+kernel
+                return clf,zone,clf.score(x_test,y_test),'SVR'+kernel,scaler
             elif num==3:
                 #Neural Net
                 clf=mlpr(hidden_layer_size=hidden_layer_size)
@@ -73,26 +75,26 @@ class Regression():
                 for i in hidden_layer_size:
                     str+='-> {}'.format(i)
                 # storeObj(clf, zone, clf.score(x_test, y_test), 'NeuralNet'+' hidden layer size'+hidden_layer_size)
-                return clf,zone,clf.score(x_test,y_test),'NeuralNet hidden_size'+str
+                return clf,zone,clf.score(x_test,y_test),'NeuralNet hidden_size'+str,scaler
             elif num==4:
                 #Gradient Boosting Regressor
                 clf=GBR(loss=loss,n_estimators=n_estimators,learning_rate=learning_rate)
                 clf.fit(x_train,y_train)
                 # storeObj(clf, zone, clf.score(x_test, y_test), 'Gradient Boosting Regressor')
-                return clf,zone,clf.score(x_test,y_test),'Gradient Boosted Regressor'
+                return clf,zone,clf.score(x_test,y_test),'Gradient Boosted Regressor',scaler
             elif num==5:
                 clf=ABR()
                 clf.fit(x_train,y_train)
                 # storeObj(clf, zone, clf.score(x_test, y_test), 'AdaBoost Regressor')
-                return clf,zone,clf.score(x_test,y_test),'AdaBoost Regressor'
+                return clf,zone,clf.score(x_test,y_test),'AdaBoost Regressor',scaler
 
     def test(self,model):
         pass
 
     @staticmethod
-    def save_model(self,obj,zone,score,name):
+    def save_model(self,obj,zone,score,preobj,name):
         du=dumps(obj)
-        fd.storeObj(pickleobj=du,zone=zone,acc=score,name=name)
+        fd.storeObj(pickleobj=du,zone=zone,acc=score,preobj=preobj,name=name)
 
     @staticmethod
     def predict(self,input_x,name,zone):
